ba41dafb81947eee78769fa09dd6d158eeedf118
gregor <gregor.olenik@web.de>
2015-06-01 10:38:40 +0200

    add DictTables

--- ToggleCodeButton.py
+++ ToggleCodeButton.py
@@ -22,3 +22,19 @@ function code_toggle() {
 $( document ).ready(code_toggle);
 </script>
 <form action="javascript:code_toggle()"><input type="submit" value="Click here to toggle on/off the raw code."></form>''')
+
+EnableEquationNumbers = NotebookObject(""" <script type="text/Javascript"> MathJax.Hub.Config({ TeX: { equationNumbers: { autoNumber: "AMS", useLabelIds: true } }});
+</script>""")
+
+class DictTable(dict):
+    # Overridden dict class which takes a dict in the form {'a': 2, 'b': 3},
+    # and renders an HTML Table in IPython Notebook.
+    def _repr_html_(self):
+        html = ["<table width=100%>"]
+        for key, value in self.iteritems():
+            html.append("<tr>")
+            html.append("<td>{0}</td>".format(key))
+            html.append("<td>{0}</td>".format(value))
+            html.append("</tr>")
+        html.append("</table>")
+        return ''.join(html)
