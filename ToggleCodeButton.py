9647dbf5c2a569203cf8a5018ae0c185c1d3f91e
gregor <gregor.olenik@web.de>
2015-06-01 10:16:41 +0200

    first commit

---
+++ ToggleCodeButton.py
@@ -0,0 +1,24 @@
+from IPython.display import HTML
+
+class NotebookObject():
+
+    def __init__(self, code_block):
+        self.code_block = code_block
+
+
+    def _repr_html_(self):
+        return self.code_block 
+
+TogleCode('''<script>
+code_show=false; 
+function code_toggle() {
+ if (code_show){
+ $('div.input').hide();
+ } else {
+ $('div.input').show();
+ }
+ code_show = !code_show
+} 
+$( document ).ready(code_toggle);
+</script>
+<form action="javascript:code_toggle()"><input type="submit" value="Click here to toggle on/off the raw code."></form>''')
