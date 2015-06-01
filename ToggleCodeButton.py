0cd425de4ee459ae9b60756359adef1e9e6a76b6
gregor <gregor.olenik@web.de>
2015-06-01 19:35:06 +0200

    add DictValueTable

--- ToggleCodeButton.py
+++ ToggleCodeButton.py
@@ -7,10 +7,10 @@ class NotebookObject():
 
 
     def _repr_html_(self):
-        return self.code_block 
+        return self.code_block
 
 ToggleCodeButton = NotebookObject('''<script>
-code_show=false; 
+code_show=false;
 function code_toggle() {
  if (code_show){
  $('div.input').hide();
@@ -18,7 +18,7 @@ function code_toggle() {
  $('div.input').show();
  }
  code_show = !code_show
-} 
+}
 $( document ).ready(code_toggle);
 </script>
 <form action="javascript:code_toggle()"><input type="submit" value="Click here to toggle on/off the raw code."></form>''')
@@ -39,5 +39,24 @@ class DictTable(dict):
             html.append("<td>{0}</td>".format(key))
             html.append("<td>{0}</td>".format(value))
             html.append("</tr>")
+        html.append("</table>")
+        return ''.join(html)
+
+class DictValueTable
+    # Overridden dict class which takes a dict in the form {'a': {'val':'foo',
+    #                                                             'label':'foo [b]'
+    #                                                        },
+    # and renders an HTML Table in IPython Notebook.
+    def __getattr__(self, item):
+        return self[item]['val']
+
+    def _repr_html_(self):
+        html = ["<table width=100%>"]
+        for key, subdict in self.iteritems():
+            for disc, value in suvdict.iteritems(): 
+                html.append("<tr>")
+                html.append("<td>{0}</td>".format(disc))
+                html.append("<td>{0}</td>".format(value))
+                html.append("</tr>")
         html.append("</table>")
         return ''.join(html)
